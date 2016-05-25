b8b0f8be0fac1edcf8c2497739a3d6f9ddf6310a
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-05-25 17:32:10 +0900

    create the role with the latest script

---
+++ ansible-role-example/spec/serverspec/default_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+require 'serverspec'
+
+require 'spec_helper'
+
+describe package('zsh') do
+  it { should be_installed }
+end 
+
+describe file('/tmp/foo') do
+  it { should exist }
+  it { should be_file }
+  its(:content) { should match /Hello world/ }
+end
